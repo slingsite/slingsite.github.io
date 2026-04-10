690af2b78fe5f5e70410396ca32d871a709724b2
damiarita <damiarita@users.noreply.github.com>
2026-04-10 22:02:26 +0000

    deploy: damiarita/slingsite@6bf70a9c85b6d4723c1e9d92a94f1f93cc8b7d8c

--- _next/static/lbJdmEtDegI24a-XRpR1U/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002F[slug]","\u002F[locale]\u002Fimage","\u002F[locale]\u002Fsuscribe","\u002F[locale]\u002Fvideo"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
